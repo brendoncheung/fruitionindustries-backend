ace25bc82211bd905a0735500a4d7ff0963e7f32
Wing Sun Cheung <brendon.cheung@outlook.com>
2023-02-09 08:44:41 -0500

    modified index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use('/api/purchasing', purchaseRoute);
 
-const connectionString = 'mongodb://localhost/fruition'
+const connectionString = 'mongodb://localhost/fruitiondb'
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
